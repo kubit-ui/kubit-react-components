c92cc8074b8b6ca21bafde3406730708a5d8ecd1
Kubit <kubit@opendigitalservices.com>
2024-07-27 07:33:43 +0200

    Update configuration and vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  esbuild: {
+    target: 'es2020',
+  },
   resolve: {
     alias: {
       '@/assets': '/src/assets',
