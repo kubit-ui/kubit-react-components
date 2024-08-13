c80f88a330388500e5dcda45c75f9f57168a0bb6
Kubit <kubit@opendigitalservices.com>
2024-08-13 08:54:50 +0200

    Include scroll styles

---
+++ src/types/styles/scroll.ts
@@ -0,0 +1,20 @@
+export type ScrollTypes = {
+  scrollbar_width?: string;
+  scrollbar_color?: string;
+  overflow_block?: string;
+  overflow_inline?: string;
+  overflow_x?: string;
+  overflow_y?: string;
+  overflow?: string;
+  overflow_clip_margin?: string;
+  scrollbar_gutter?: string;
+  scroll_behavior?: string;
+  scroll_margin?: string;
+  scroll_padding?: string;
+  scroll_snap_align?: string;
+  scroll_snap_stop?: string;
+  scroll_snap_type?: string;
+  webkit_scrollbar?: string; // ::-webkit-scrollbar pseudo-element
+  scroll_container?: string; // scroll container glossary term
+  scrollbar_aria_role?: string; // scrollbar ARIA role
+};
